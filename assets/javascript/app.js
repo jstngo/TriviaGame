5b4d5547ef99003f441dac4176a3b15be4867179
Justin Goyena <jgoyen@Justins-MBP.attlocal.net>
2019-07-20 22:48:29 -0700

    added gif pages, added next page function, created new timer for gif page, added 6 forms and 6 will ferrell gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
-var counter = 6;
+var counter = 60;
 $('span').text(counter);
 
-var timeOut = setInterval(decreaseCounter, 1*1000);
-
+var gifCounter = 5;
 var correct = "";
 
 function decreaseCounter(){
@@ -16,23 +15,65 @@ function decreaseCounter(){
 
 }
 
+function nextPage(){
+    gifCounter--;
+    gifTime = setInterval(nextPage, 1*1000);
+    $("#one").hide();
+    $("#gifOne").show();
 
-$('#play').on('click', function(){
-    timeOut = setInterval(decreaseCounter, 1*1000);
-})
-
-$('#pause').on('click', function(){
-    clearInterval(timeOut);
-})
+    if (gifCounter == 0){
+        $("#gifOne").hide();
+        $("#two").show();
+    }
+}
 
 
 $("input[type='button']").on('click', function(){
+    ($(this).attr('data-type') == 'start')
+    timeOut = setInterval(decreaseCounter, 1*1000);
+    $("#startPage").hide();
+    $("#one").show();
+
+
     if ($(this).attr('data-type') == 'correct'){
         console.log(correct);
-        $('#content').text('YOU DID IT');
+        $('#content').text('SUPER DUPER');
         clearInterval(timeOut);
-    }else{
+        nextPage();
+
+    }
+    
+    if ($(this).attr('data-type') == 'wrong'){
         $('#content').text('incorrect')
         clearInterval(timeOut);
+        nextPage();
     }
+
+
+
 })
+
+
+
+$(document).ready(function(){
+    
+    $("#startPage").show();
+
+      $("#one").hide();
+    $("#gifOne").hide();
+
+      $("#two").hide();
+    $("#gifTwo").hide();
+
+      $("#three").hide();
+    $("#gifThree").hide();
+
+      $("#four").hide();
+    $("#gifFour").hide();
+
+      $("#five").hide();
+    $("#gifFive").hide();
+
+      $("#six").hide();
+    $("#gifSix").hide();
+    });
