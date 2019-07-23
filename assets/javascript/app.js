dcd1df8b1f87edba8de7d37876e561a4056fab85
Justin Goyena <jgoyen@Justins-MBP.attlocal.net>
2019-07-22 23:54:35 -0700

    added multiple functions for gif timers and form page timers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,171 @@
-var counter = 60;
-$('span').text(counter);
+var counter1 = 30;
+console.log(counter1);
+
+var counter2 = 30;
+
 
 var gifCounter = 5;
+
 var correct = "";
 
-function decreaseCounter(){
-    counter--;
-    $('span').text(counter);
+var a = 0;
 
-    if (counter == 0){
-        clearInterval(timeOut);
-        $('#content').text('timer is up!')
+var b = 0;
+
+// var formPages = ["$('#one')", "$('#two')", "$('#three')", "$('#four')", "$('#five')", "$('#six')"];
+// var formPages = [$('#one'), $('#two'), $('#three'), $('#four'), $('#five'), $('#six')];
+
+// var gifPages = ["$('#gifOne')", "$('#gifTwo')", "$('#gifThree')", "$('#gifFour')", "$('#gifFive')", "$('#gifSix')"];
+// var gifPages = ["$('#gifOne')", "$('#gifTwo')", "$('#gifThree')", "$('#gifFour')", "$('#gifFive')", "$('#gifSix')"];
+
+
+
+// function decreaseCounter1(){
+    
+// timeOut1 = setInterval(decreaseCounter1, 1*1000);
+// // timeOut1 = Math.floor((distance % (1000 * 60)) / 1000);
+
+//     if (counter1 > 0){
+
+//         counter1--;
+//         $('#timerOne').text(counter1);
+
+//     }else if(counter1 == 0){
+//         clearInterval(timeOut1);
+//         // clearTimeout(timeOut);
+//         // counter1 = 30;
+//     }
+
+// }
+
+// function decreaseCounter2(){
+    
+//     var timeOut2 = setInterval(decreaseCounter2, 1*1000);
+
+//     if (counter2 > 0){
+//         counter2--;
+//         $('#timerTwo').text(counter2);
+
+      
+//     }else if (counter2 == 0){
+//   clearInterval(timeOut2);
+        
+//         counter2 = 30;
+//     }
+
+// }
+
+
+
+
+function decreaseGifCounter(){
+
+gifTime = setInterval(decreaseGifCounter, 1*1000);
+
+    if (gifCounter > 0){
+        gifCounter--;  
+        console.log(gifCounter);
+
+    
+        // clearTimeout(gifTime);
     }
-
+    else if (gifCounter == 0){
+        formPage();
+        $('#gifOne').hide();
+        $("#gifTwo").hide();
+        $("#gifThree").hide();
+        $("#gifFour").hide();
+        $("#gifFive").hide();
+        $("#gifSix").hide();
+        b++;
+        clearInterval(gifTime);
+        // return gifCounter = 5;
+    }
+  
+    
 }
 
-function nextPage(){
-    gifCounter--;
-    gifTime = setInterval(nextPage, 1*1000);
-    $("#one").hide();
-    $("#gifOne").show();
+function gifPage(){
+    
+    decreaseGifCounter();
+if (b == 0){
 
-    if (gifCounter == 0){
-        $("#gifOne").hide();
-        $("#two").show();
-    }
+    
+    $('#gifOne').show();
+    $('#one').hide();
+
+}if (b == 1){
+  
+
+    $("#gifTwo").show();
+    $('#two').hide();
+
 }
+}
+var decreaseCounter1 = setInterval(function() {
+    counter1--;
+    $('#timerOne').text(counter1);
+    // Display 'counter' wherever you want to display it.
+    if (counter1 == 0) {
+        // Display a login box
+        clearInterval(decreaseCounter1);
+    }
+}, 1000);
 
+function formPage(){
+
+
+if (a == 0){
+decreaseCounter1;
+    $('#one').show();
+
+}else if (a == 1){
+    decreaseCounter1;
+    $('#two').show();
+
+}else if (a == 2){
+    $('#three').show();
+}
+}
 
 $("input[type='button']").on('click', function(){
-    ($(this).attr('data-type') == 'start')
-    timeOut = setInterval(decreaseCounter, 1*1000);
-    $("#startPage").hide();
-    $("#one").show();
+    if ($(this).attr('data-type') == 'start'){
 
+         $("#startPage").hide();
+         formPage();
+
+         $('#one').show();
+         event.preventDefault();
+    }
 
     if ($(this).attr('data-type') == 'correct'){
-        console.log(correct);
         $('#content').text('SUPER DUPER');
-        clearInterval(timeOut);
-        nextPage();
 
+        // clearTimeout(timeOut1);
+        // clearTimeout(timeOut1);
+        clearInterval(decreaseCounter1);
+        gifPage();
+        a++;
     }
     
     if ($(this).attr('data-type') == 'wrong'){
         $('#content').text('incorrect')
-        clearInterval(timeOut);
-        nextPage();
+        // clearInterval(timeOut1);
+        // clearTimeout(timeOut);
+        clearInterval(decreaseCounter1);
+        gifPage();
+        a++;
     }
 
-
+    event.preventDefault();
 
 })
 
-
-
 $(document).ready(function(){
-    
+
     $("#startPage").show();
 
-      $("#one").hide();
+    $("#one").hide();
     $("#gifOne").hide();
 
       $("#two").hide();
@@ -76,4 +182,5 @@ $(document).ready(function(){
 
       $("#six").hide();
     $("#gifSix").hide();
-    });
+    
+    })
