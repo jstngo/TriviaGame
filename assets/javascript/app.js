dce2437229678b6079913fff62d1dc9940dd5f6a
Justin Goyena <jgoyen@Justins-MBP.attlocal.net>
2019-07-26 22:05:29 -0700

    added gif timer and form page timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,41 @@
-var counter1 = 10;
-console.log(counter1);
+// var counter1 = 10;
+// console.log(counter1);
 
-var counter2 = 30;
+// var counter2 = 30;
 
-
+var counter = 30;
 var gifCounter = 5;
+
+var nextCounter = 30;
+
+var timer;
+var gifTimer;
 
 var correct = "";
 
-var a = 0;
+var f = 0;
+
 
 var b = 0;
+console.log(b);
+var a = 0;
 
-// var formPages = ["$('#one')", "$('#two')", "$('#three')", "$('#four')", "$('#five')", "$('#six')"];
-// var formPages = [$('#one'), $('#two'), $('#three'), $('#four'), $('#five'), $('#six')];
+var questions = { "first": "This burrito is delicious but it is filling", "second": "Todd, where's my goddamn latte!?", "third": "Help me Jesus, help me Jewish God, help me Allah, HELP ME TOM CRUISE", "fourth": "I swear, I'm so pissed off at my mom. As soon as she's of age, I'm putting her in a home.", "fith": "Maybe we should call ourselves the febreeze brothers because things are feeling so fresh right now", "sixth": "I'm in a store, and I'm singing" };
 
-// var gifPages = ["$('#gifOne')", "$('#gifTwo')", "$('#gifThree')", "$('#gifFour')", "$('#gifFive')", "$('#gifSix')"];
-// var gifPages = ["$('#gifOne')", "$('#gifTwo')", "$('#gifThree')", "$('#gifFour')", "$('#gifFive')", "$('#gifSix')"];
+var answers = { "row1": ["Old School", "A Night at the Roxburry", "Talladega Nights", "Blades of Glory", " Other Guys", "Get Hard"], "row2": ["Anchorman", "Lego Movie", "Kicking and Screaming", "Land of the Lost", "Megamind", "Elf"], "row3": ["Get Hard", "Everything Must Go", "The Campaign", "Step Brothers", "Stranger than Fiction", "Kicking and Screaming"], "row4": ["Blades of Glory", "Zoolander", "Casa Padre", "Semi-Pro", "Austin Powers", "Lego Movie 2"] };
 
+var correct = { "answers": ["Anchorman", "Zoolander", "Talladega Nights", "Step Brothers", "Other Guys", "Elf"] };
 
+var start = {"Timer": function formTimer(){setInterval}};
 
-// function decreaseCounter1(){
-    
-// timeOut1 = setInterval(decreaseCounter1, 1*1000);
-// // timeOut1 = Math.floor((distance % (1000 * 60)) / 1000);
+function decreaseGifCounter() {
 
-//     if (counter1 > 0){
+    gifTime = setInterval(function (){
 
-//         counter1--;
-//         $('#timerOne').text(counter1);
-
-//     }else if(counter1 == 0){
-//         clearInterval(timeOut1);
-//         // clearTimeout(timeOut);
-//         // counter1 = 30;
-//     }
-
-// }
-
-// function decreaseCounter2(){
-    
-//     var timeOut2 = setInterval(decreaseCounter2, 1*1000);
-
-//     if (counter2 > 0){
-//         counter2--;
-//         $('#timerTwo').text(counter2);
-
-      
-//     }else if (counter2 == 0){
-//   clearInterval(timeOut2);
-        
-//         counter2 = 30;
-//     }
-
-// }
-
-
-
-
-function decreaseGifCounter(){
-   
-gifTime = setInterval(decreaseGifCounter, 1*1000);
-
-    if (gifCounter > 0){
-        gifCounter--;  
+        gifCounter--;
         console.log(gifCounter);
-
-        
         // clearTimeout(gifTime);
-    }
-    else if (gifCounter == 0){
+        if (gifCounter == 0) {
         formPage();
         $('#gifOne').hide();
         $("#gifTwo").hide();
@@ -77,136 +43,154 @@ gifTime = setInterval(decreaseGifCounter, 1*1000);
         $("#gifFour").hide();
         $("#gifFive").hide();
         $("#gifSix").hide();
-        b++;
+        // b++;
+        a++;
         clearInterval(gifTime);
         // return gifCounter = 5;
     }
-  
-    
+}, 1000)
 }
 
-function gifPage(){
-    
-    decreaseGifCounter();
-if (b == 0){
+// function decreaseGifCounter() {
 
-    
-    $('#gifOne').show();
-    $('#one').hide();
+//     gifTime = setInterval(decreaseGifCounter, 1 * 1000);
 
-}if (b == 1){
-  
+//     if (gifCounter > 0) {
+//         gifCounter--;
+//         console.log(gifCounter);
+//         // clearTimeout(gifTime);
+//     }
+//     else if (gifCounter == 0) {
+//         formPage();
+//         $('#gifOne').hide();
+//         $("#gifTwo").hide();
+//         $("#gifThree").hide();
+//         $("#gifFour").hide();
+//         $("#gifFive").hide();
+//         $("#gifSix").hide();
+//         // b++;
+//         a++;
+//         clearInterval(gifTime);
+//         // return gifCounter = 5;
+//     }
+// }
+function formTimer() { 
 
-    $("#gifTwo").show();
-    $('#two').hide();
+    timer = setInterval(function () { //starts the countdown timer
+        counter--;
+        $('#formTime').text(counter);
 
-}
-}
-
-function formCounter1(){
-
-var decreaseCounter1 = setInterval(function() {
-    counter1--;
-    $('#timerOne').text(counter1);
-    // Display 'counter' wherever you want to display it.
-    
-    if (counter1 == 0) {
-        console.log(counter1)
-        // clearFormCounter()
-        // replayFormCounter()
-        // Display a login box
-        clearInterval(decreaseCounter1);
-        // stopFormCounter;
-    }
-}, 1000);
-}
-
-function formCounter2(){
-
-    var decreaseCounter2 = setInterval(function() {
-        counter2--;
-        $('#timerTwo').text(counter2);
-        // Display 'counter' wherever you want to display it.
-        if (counter2 == 0) {
-            // clearFormCounter()
-            // replayFormCounter()
-            // Display a login box
-            clearInterval(decreaseCounter2);
-            // stopFormCounter;
+        if (counter == 0) {
+            clearInterval(timer);
         }
-    }, 1000);
-    }
-
-function formPage(){
-
-
-if (a == 0){
-// formCounter();
-    $('#one').show();
-formCounter1();
-}else if (a == 1){
-// formCounter();
-    $('#two').show();
-formCounter1();
-}else if (a == 2){
-    $('#three').show();
+    }, 1000)
 }
+function gifPage() {
+
+    decreaseGifCounter();
+
+    if (b == 0) {
+    formTimer();
+        $('#gifOne').show();
+        $('#one').hide();
+ 
+    }if (b == 1) {
+        formTimer();
+        $("#gifTwo").show();
+        $('#one').hide();
+
+    }if (b == 2) {
+        formTimer();
+        $("gifThree").show();
+        $('#one').hide();
+    }if (b == 3) {
+        formTimer();
+        $("gifFour").show();
+        $('#one').hide();
+    }if (b == 4) {
+        formTimer();
+        $("gifFive").show();
+        $('#one').hide();
+    }if (b == 5) {
+        formTimer();
+        $("gifSix").show();
+        $('#one').hide();
+    }
 }
 
-$("input[type='button']").on('click', function(){
-    if ($(this).attr('data-type') == 'start'){
-
-         $("#startPage").hide();
-         formPage();
-
-         $('#one').show();
-         event.preventDefault();
-    }
-
-    if ($(this).attr('data-type') == 'correct'){
-        $('#content').text('SUPER DUPER');
-
-        // clearTimeout(timeOut1);
-clearInterval(formCounter1);
-
-        gifPage();
-        a++;
-    }
+function formPage() {
     
-    if ($(this).attr('data-type') == 'wrong'){
-        $('#content').text('incorrect')
-        // clearInterval(timeOut1);
-        // clearTimeout(timeOut);
-        clearInterval(formCounter1);
-        // clearInterval(formCounter2);
-        gifPage();
-        a++;
+    $('#one').show();
+    $("input[data-type='answer1']").val(answers.row1[f]);
+    $("input[data-type='answer2']").val(answers.row2[f]);
+    $("input[data-type='answer3']").val(answers.row3[f]);
+    $("input[data-type='answer4']").val(answers.row4[f]);
+
+};
+
+$("input[type='button']").on('click', function () {
+
+    if ($(this).attr('data-type') == 'start') {
+
+        $("#startPage").hide();
+
+        formPage();
+
+        $('#one').show();
+
+
+        formTimer();
+
+        event.preventDefault();
+    } else {
+        // for (var i=0; i<correct.answers.length; i++);
+        if ($(this).attr('value') == correct.answers[a]) {
+            $('#content').text('SUPER DUPER');
+            f++;
+            clearInterval(timer);
+            counter = 33;
+            gifCounter = 5;
+            $('#formTime').text(counter);
+            gifPage();
+            b++;
+
+        }
+        // for (var i=0; i<correct.answers.length; i++);
+        if ($(this).attr('value') != correct.answers[a]) {
+            $('#content').text('incorrect')
+            f++;
+            clearInterval(timer);
+            gifPage();
+            counter = 33;
+            gifCounter = 5;
+            $('#formTime').text(counter);
+            b++;
+        }
+
+        event.preventDefault();
     }
-
-    event.preventDefault();
-
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     $("#startPage").show();
 
     $("#one").hide();
     $("#gifOne").hide();
 
-      $("#two").hide();
+    $("#two").hide();
     $("#gifTwo").hide();
 
-      $("#three").hide();
+    $("#three").hide();
     $("#gifThree").hide();
 
-      $("#four").hide();
+    $("#four").hide();
     $("#gifFour").hide();
 
-      $("#five").hide();
+    $("#five").hide();
     $("#gifFive").hide();
 
-      $("#six").hide();
+    $("#six").hide();
     $("#gifSix").hide();
-    
-    })
+
+})
